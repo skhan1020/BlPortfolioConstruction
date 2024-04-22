bf28af3ef9443067d68ff4045a036bee564d5f49
skhan1020 <souratosh.khan86@gmail.com>
2024-04-22 14:38:33 -0400

    added docstrings

--- black_litterman_model.py
+++ black_litterman_model.py
@@ -5,6 +5,28 @@ class BlackLittermanModel:
     def __init__(self, cov, weights, risk_aversion, T, position_matrix, return_vector):
         """
         Implementation of the Black-Litterman Model
+
+        Parameters
+        ----------
+
+        cov : pd.DataFrame
+            Covariance Matrix of asset returns 
+        
+        weights : np.array
+            Optimized portfolio allocations (prior)
+        
+        risk_aversion : float
+            Investor's risk appetite
+        
+        T : int
+            Number of historical data points
+        
+        position_matrix : np.ndarray
+            Positions taken by investor based on Absolute & Relative Views
+
+        return_vector : np.array
+            Expected confidence scores associated with each view of the investor
+        
         """
 
         self.cov = cov
@@ -17,25 +39,50 @@ class BlackLittermanModel:
     
     @property
     def excess_return(self):
-    
-        # Equilibrium of Excess Return - Prior View on Risk Premia
+        """
+        
+        Equilibrium of Excess Return - Prior View on Risk Premia
+        
+        """
+        
         return self.risk_aversion*np.dot(self.cov, self.weights)
 
     @property
     def prior_covariance(self):
+        """
+        
+        Prior view on Covariance
 
+        """
         return self.tau * self.cov
     
     @property
     def prior_distbn_excess_ret(self):
+        """
+        
+        Prior distribution of excess return
+
+        """
+
         return np.random.normal(loc=self.excess_return, scale=self.excess_return, size=1)
     
     @property
     def omega(self):
+        """
+        
+        He & Litterman's definition of covariance matrix : Omega
+
+        """
+
         return np.diag(np.diag(multi_dot([self.P, self.prior_covariance, self.P.T])))
     
     @property
     def posterior_return(self):
+        """
+
+        Mean of the Posterior distribution of excess returns
+    
+        """
         
         A = np.linalg.inv(np.linalg.inv(self.prior_covariance) + multi_dot([self.P.T, np.linalg.inv(self.omega), self.P]))
         
@@ -48,6 +95,21 @@ class BlackLittermanModel:
 def calc_asset_allocations(risk_aversion, cov, posterior_returns):
     """
     Compute Updated Allocations using Posterior Distribution
+    
+    Parameters
+    ----------
+
+    risk_aversion : float
+        Risk aversion of Investor
+
+    cov : pd.DataFrame
+        Historical covariance matrix of asset returns
+    
+    Returns
+    -------
+
+    Updated portfolio allocations based on posterior distribution of excess asset returns
+
     """
     return (1 / risk_aversion) * (np.dot(np.linalg.inv(cov), posterior_returns))
 
