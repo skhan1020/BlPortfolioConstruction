e5239a2f561bd2bae7225118af8ca8e3fe1b1311
skhan1020 <souratosh.khan86@gmail.com>
2024-07-08 00:38:14 -0400

    added docstrings, remove commented lines, specify method to volatility in efficient frontier computation

--- blportopt/portfolio_construction.py
+++ blportopt/portfolio_construction.py
@@ -8,15 +8,11 @@ from blportopt.data_utils import (
     FamaFrenchFactorDataLoader,
     EquityDataLoader,
 )
-from blportopt.optimizer import (
-    PortoflioOptimizer,
-    MeanVarOptimizer,
-    MaxSharpeRatioOptimizer,
-)
+from blportopt.optimizer import PortoflioOptimizer
 
 def portfolio_data(tickers, freq=12):
     """
-    Determine Excess Asset Returns (Historical) and Risk-Free Rates from Factor Data for all equities
+    Determine Excess Asset Returns (Historical), Standard Deviation, Covariance Matrix and Risk-Free Rates for all equities
 
     Parameters
     ----------
@@ -79,11 +75,6 @@ def portfolio_data(tickers, freq=12):
 
 
     return freq_returns, freq_stdev, cov, freq_rf
-     # annual_asset_returns = asset_rf_data.groupby(["Year"])[tickers].mean().reset_index()
-    # monthly_asset_returns.set_index("Year", inplace=True)
-
-    # return monthly_asset_returns
-
 
 
 def calc_optimal_portfolio_weights(mu, cov, rf, tr, risk_aversion, method):
@@ -126,15 +117,39 @@ def calc_optimal_portfolio_weights(mu, cov, rf, tr, risk_aversion, method):
 
 
 
-def efficient_frontier(mu, cov, rf, risk_aversion, method):
+def efficient_frontier(mu, cov, rf, risk_aversion):
+    """
+    Constructing Efficient Frontier by optimizing portfolios across a range of target returns
 
+    Parameters
+    ----------
+
+    mu : pd.Series
+        Average excess annual returns of assets within portfolio
+
+    cov : pd.DataFrame
+        Covariance Matrix
+
+    rf : float
+        Average risk-free rate from historical 'RF' data
+    
+    risk_aversion : float
+        Investor risk appetite
+    
+    Returns
+    -------
+
+    efport : pd.DataFrame
+        Pandas dataframe with target returns, computed target volatilites from optimization process, and target sharpe ratios
+
+    """
     target_returns = np.linspace(mu.min(), mu.sum(), 100)
     tvols = []
     for tr in target_returns:
         
         port_optim = PortoflioOptimizer(mu=mu, cov=cov, tr=tr, rf=rf, risk_aversion=risk_aversion)
         
-        opt_ef = port_optim.optimize(method=method)
+        opt_ef = port_optim.optimize(method="volatility")
         tvols.append(opt_ef["fun"])
 
 
